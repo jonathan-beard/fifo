d36b86c8451355c67ab43e781a00b1f108cedca2
Jonathan Beard <jbeard@wustl.edu>
2014-08-13 09:45:05 -0500

    updates

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ typedef RingBuffer< std::int64_t,
                     false > TheBuffer;
 #elif defined USELOCAL
 typedef RingBuffer< std::int64_t          /* buffer type */,
-                    RingBufferType::Heap  /* allocation type */,
+                    RingBufferType::Infinite /* allocation type */,
                     true                  /* turn on monitoring */ >  TheBuffer;
 #endif
 
@@ -49,7 +49,7 @@ void
 producer( Data &data, TheBuffer &buffer )
 {
    std::int64_t current_count( 0 );
-   const float serviceTime( 10e-6 );
+   const float serviceTime( 5e-6 );
    while( current_count++ < data.send_count )
    {
       auto &ref( buffer.allocate() );
@@ -69,7 +69,7 @@ void
 consumer( TheBuffer &buffer )
 {
    std::int64_t   current_count( 0 );
-   const float serviceTime( 5e-6 );
+   const float serviceTime( 1e-6 );
    RBSignal signal( RBSignal::NONE );
    while( signal != RBSignal::RBEOF )
    {
