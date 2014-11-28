ebf6f9905a3c9e9be7a5493f8ec52b57bd684865
jonathan-beard <jonathan.c.beard@gmail.com>
2014-11-28 13:50:38 -0600

    updated runtest.pl

--- runtest.pl
+++ runtest.pl
@@ -2,9 +2,19 @@
 use strict;
 use warnings;
 
-my $count = 0;
-for( $count < 10 )
+my $timesuffix = ".0e-6";
+my $machine    = `uname -n`;
+$machine =~ tr/\./_/;
+
+
+for my $times( 1 ... 10 )
 {
-   my $logfile = "/project/mercury/svardata/log_5eNeg10_".$count;
-   `./ringb 2> $logfile`;
+   for my $count( 0 ... 19 )
+   {
+      my $cmd = "ringb 10.0e-6 ".$times.$timesuffix." 1000000000";
+      my $file = $cmd;
+      $file =~ tr/ /_/;
+      $file .= "_".$machine;
+      print "./".$cmd." 2> /project/mercury/svardata/servicedata/$file\_$count.dat \n";
+   }
 }
